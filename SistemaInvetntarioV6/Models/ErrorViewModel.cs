83868232cf34a0bf2f229cdd02480fab0153638d
JesusDanielP <danicho293@gmail.com>
2023-10-01 14:11:27 -0600

    Crear Proyecto

---
+++ SistemaInvetntarioV6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaInvetntarioV6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
